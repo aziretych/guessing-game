87e0916c75d7f314e3f14a6b60d2acfe1516092f
aziretych <aziretpochta@gmail.com>
2026-05-28 09:30:00 +0000

    added README and the main class

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# guessing-game
+# Number Guess Game
+
+Simple Python number guessing game using classes and multiple files.
+
+## Features
+
+- Random number generation from 1 to 99
+- Score system
+- Hint system
+- Exit command
+
+## How to Run
+
+```bash
+python main.py
--- main.py
+++ main.py
@@ -1,41 +1,9 @@
-import random
+from game import GuessGame
 
 
 def main():
-    print("The game is started")
-
-    num = random.randint(1,99)
-    score = 10
-    while True:     
-        users_number = input("Guess the number: ")
-        if users_number == 'exit':
-            
-            break
-        elif users_number== 'is the number even':
-            if num % 2 ==0 :
-                print('The number is even')
-            else:
-                print("the number is odd ")
-        elif num == users_number:
-            print("You won")
-            print(f'Your score {score}')
-        else: 
-            print("You lose")
-        score -= 1
-        
-
-    
-
-    # TODO: Features 
-    # use loop
-    # take input for finishing the game
-    # take input for hint question 
-    # add scoring mechanism 
-
-    # TODO: Refactor
-    # extract classes
-    # use separate files
-
+    game = GuessGame()
+    game.start()
 
 
 if __name__ == "__main__":
